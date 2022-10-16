916b205f15991dc9c259101535a291e419e6d654
Hrithik Singh <hrithikks10@gmail.com>
2022-10-16 15:44:21 +0530

    commited

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  
+  
+  
+  
+  
+  reactStrictMode: true,
+  images: {
+    domains: ["pyxis.nymag.com", "lh3.googleusercontent.com"],
+  },
+  // swcMinify: true,
+};
+
+module.exports = nextConfig;
